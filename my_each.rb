ea9e239dbfed103c54c9306e38c76976b17bf7f7
Luke Granger <grangerl330@gmail.com>
2019-01-05 17:24:28 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,9 @@
 def my_each(collection)
-  if block_given?
     i = 0 
     while i < collection.length 
       yield collection[i]
       i = i + 1 
     end 
     collection 
-  else 
-    "No block given"
   end 
 end
